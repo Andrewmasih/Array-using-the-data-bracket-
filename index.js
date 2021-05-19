7ec7cac2c7af4fdf1811abd650d6eabfc7f6b927
Andrew Masih <armasih@outlook.com>
2021-05-19 00:51:15 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+// a list of values that can be stores many values in 1 varible, arrays can be created by using Literal Notation
+
+let myArray = [1, 2, "apples", "oranges"];
+ // we can declare a new varible and assign the data in the [ ] brackets. the data in the brackets can be any data type you want, strings, numbers, booleans and even arrays
+
+
+console.log (myArray);
+
